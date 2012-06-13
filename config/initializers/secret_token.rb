0ca3c9b1aca1c4a3b234c66071d94b864d473700
Taldor <perlyk@ubuntu.ubuntu-domain>
2012-06-13 10:18:06 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Licht::Application.config.secret_token = '5306f0b4bb764bfd11f8618e8b2133601d8c8a3c0fb668215a4511bc1a910b7dda5d62cabf07c2cc70a57224c216b23cbd590f8eb6df235ea9caa3ec41222cb1'
